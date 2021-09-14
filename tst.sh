34076b283b37f9d7cbf5c766344d936b174bc1ed
Nightshade02 <89810478+Nightshade02@users.noreply.github.com>
2021-09-14 19:00:59 +0300

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #!/bin/sh
-#Code:Yaman Efkar
-#Türk Siber Timi
-#İnstagram : @turksibertimi
-#Youtube : YamanEfkar
+#Code:Ahmet Can
+#Apex Hack 
+
+
+
+#Youtube : 
 clear
 case $1 in
 
@@ -63,9 +65,9 @@ echo -e '\033[31;40;1m
 	────────────────█████████───────────────
 	──────────────█████████████─────────────
 
-	   \e[35m☾★ ☾★ ☾★ ☾★ YamanEfkar ☾★ ☾★ ☾★ ☾★	
+	   \e[35m☾★ ☾★ ☾★ ☾★ Ahmet Can ☾★ ☾★ ☾★ ☾★	
 	 \e[31m☾★ ☾★ ☾★ ☾★ Türk Siber Timi ☾★ ☾★ ☾★ ☾★	
-  \e[32m☾★ ☾★ ☾★ ☾★ İnstagram : @turksibertimi ☾★ ☾★ ☾★ ☾★	
+  \e[32m☾★ ☾★ ☾★ ☾★ İnstagram : @Apez Hack Team ☾★ ☾★ ☾★ ☾★	
 '
 echo -e '
 \e[31m[01]\e[32mİnstagram			\e[31m[04]\e[32mFacebook
